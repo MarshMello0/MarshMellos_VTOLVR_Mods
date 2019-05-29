daf1a9c9bac64da7b81af0baf7bf5dcd6801040e
Ben Wilson <33008390+MarshMello0@users.noreply.github.com>
2019-05-29 19:40:58 +0100

    First Commit

---
+++ TestingMod/TestingMod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TestingMod
+{
+    public class Load
+    {
+        public static void Init()
+        {
+            new GameObject("Testing Mod", typeof(TestingMod));
+        }
+    }
+
+    public class TestingMod : MonoBehaviour
+    {
+        private void Start()
+        {
+            StartCoroutine(Delay());
+        }
+
+        IEnumerator Delay()
+        {
+            yield return new WaitForSeconds(2);
+            Debug.Log(Application.unityVersion);
+        }
+    }
+}
