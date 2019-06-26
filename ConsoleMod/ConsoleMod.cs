21c9d4564512b7ecf59a0f5b9f202ffff85e01d7
Ben Wilson <33008390+MarshMello0@users.noreply.github.com>
2019-06-26 22:35:48 +0100

    Console Mod, Fixed Errors with input

--- ConsoleMod/ConsoleMod.cs
+++ ConsoleMod/ConsoleMod.cs
@@ -21,7 +21,6 @@ namespace ConsoleMod
     {
         public static Console _instance;
         Windows.ConsoleWindow console = new Windows.ConsoleWindow();
-        Windows.ConsoleInput input = new Windows.ConsoleInput();
 
         string strInput;
 
@@ -43,8 +42,6 @@ namespace ConsoleMod
 
             console.Initialize();
             console.SetTitle("VTOL VR Console");
-
-            input.OnInputText += OnInputText;
 
             Application.logMessageReceived += HandleLog;
 
@@ -78,18 +75,6 @@ namespace ConsoleMod
             //    input.ClearLine();
 
             System.Console.WriteLine(message);
-
-            // If we were typing something re-add it.
-            input.RedrawInputLine();
-        }
-
-        //
-        // Update the input every frame
-        // This gets new key input and calls the OnInputText callback
-        //
-        void Update()
-        {
-            input.Update();
         }
 
         //
@@ -172,91 +157,5 @@ namespace Windows
 
         [DllImport("kernel32.dll")]
         static extern bool SetConsoleTitle(string lpConsoleTitle);
-    }
-
-    public class ConsoleInput
-    {
-        //public delegate void InputText( string strInput );
-        public event System.Action<string> OnInputText;
-        public string inputString;
-
-        public void ClearLine()
-        {
-            Console.CursorLeft = 0;
-            Console.Write(new String(' ', Console.BufferWidth));
-            Console.CursorTop--;
-            Console.CursorLeft = 0;
-        }
-
-        public void RedrawInputLine()
-        {
-            if (inputString.Length == 0) return;
-
-            if (Console.CursorLeft > 0)
-                ClearLine();
-
-            System.Console.ForegroundColor = ConsoleColor.Green;
-            System.Console.Write(inputString);
-        }
-
-        internal void OnBackspace()
-        {
-            if (inputString.Length < 1) return;
-
-            inputString = inputString.Substring(0, inputString.Length - 1);
-            RedrawInputLine();
-        }
-
-        internal void OnEscape()
-        {
-            ClearLine();
-            inputString = "";
-        }
-
-        internal void OnEnter()
-        {
-            ClearLine();
-            System.Console.ForegroundColor = ConsoleColor.Green;
-            System.Console.WriteLine("> " + inputString);
-
-            var strtext = inputString;
-            inputString = "";
-
-            if (OnInputText != null)
-            {
-                OnInputText(strtext);
-            }
-        }
-
-        public void Update()
-        {
-            if (!Console.KeyAvailable) return;
-            var key = Console.ReadKey();
-
-            if (key.Key == ConsoleKey.Enter)
-            {
-                OnEnter();
-                return;
-            }
-
-            if (key.Key == ConsoleKey.Backspace)
-            {
-                OnBackspace();
-                return;
-            }
-
-            if (key.Key == ConsoleKey.Escape)
-            {
-                OnEscape();
-                return;
-            }
-
-            if (key.KeyChar != '\u0000')
-            {
-                inputString += key.KeyChar;
-                RedrawInputLine();
-                return;
-            }
-        }
     }
 }
