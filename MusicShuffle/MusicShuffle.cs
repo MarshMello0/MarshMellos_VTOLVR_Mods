0211fe31b32c6aa5d5e0d2cfeabc387a7a15667f
Ben Wilson <33008390+MarshMello0@users.noreply.github.com>
2019-11-09 09:36:34 +0000

    Music Shuffle Mod

---
+++ MusicShuffle/MusicShuffle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Harmony;
+using System.Reflection;
+using Random = System.Random;
+
+public class MusicShuffle : VTOLMOD
+{
+    private static readonly string harmonyID = "marsh.musicshuffle";
+
+    private void Start()
+    {
+        HarmonyInstance instance = HarmonyInstance.Create(harmonyID);
+        instance.PatchAll(Assembly.GetExecutingAssembly());
+    }
+
+    public static List<T> Shuffle<T>(List<T> list)
+    {
+        Random rnd = new Random();
+        for (int i = 0; i < list.Count; i++)
+        {
+            int k = rnd.Next(0, i);
+            T value = list[k];
+            list[k] = list[i];
+            list[i] = value;
+        }
+        return list;
+    }
+}
+
+
+
+[HarmonyPatch(typeof(CockpitRadio))]
+[HarmonyPatch("Start")]
+public class Patch
+{
+    public static void Postfix()
+    {
+        CockpitRadio cockpitRadio = GameObject.FindObjectOfType<CockpitRadio>();
+        if (!cockpitRadio)
+        {
+            Debug.Log("Couldn't find the cockpit radio");
+            return;
+        }
+        List<string> songs = (List<string>)Traverse.Create(cockpitRadio).Field("origSongs").GetValue();
+
+        Random rnd = new Random();
+        songs = MusicShuffle.Shuffle(songs);
+        Traverse.Create(cockpitRadio).Field("shuffledSongs").SetValue(songs);
+        Traverse.Create(cockpitRadio).Field("origSongs").SetValue(songs);
+        Debug.Log("Music Shuffle: Reordered " + songs.Count + " songs");
+    }
+}
---
+++ MusicShuffle/README.md
@@ -0,0 +1,5 @@
+# [Music Shuffle](https://vtolvr-mods.com/mod.php?uid=2)
+
+Shuffles the music every time you launch into a mission so you're not stuck listening to the same songs are the start
+
+[![Music Shuffle Banner](https://vtolvr-mods.com/files/mods/2/banner.png)](https://vtolvr-mods.com/mod.php?uid=2)
