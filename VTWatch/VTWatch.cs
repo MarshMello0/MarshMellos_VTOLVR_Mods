f89ef2b5a7503966c394231a8a6665335da73e5b
Ben Wilson <33008390+MarshMello0@users.noreply.github.com>
2020-01-31 14:35:00 +0000

    :tada: VTWatch added

---
+++ VTWatch/VTWatch.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class VTWatch : VTOLMOD
+{
+    private TextMeshPro currentText;
+    private GameObject currentClock;
+    private bool updateText;
+    private TimeSpan lastTime;
+    public static UnityAction<bool> callback;
+    public override void ModLoaded()
+    {
+        updateText = false;
+        VTOLAPI.SceneLoaded += SceneLoaded;
+        ReadyRoom();
+
+        callback += Test;
+        Settings setting = new Settings(this);
+        setting.CreateBoolSetting("My Bool", callback);
+        VTOLAPI.CreateSettingsMenu(setting);
+        Log("Loaded!");
+    }
+
+    private void Test(bool arg0)
+    {
+        Log("Bool Setting Pressed " + arg0);
+    }
+
+    private GameObject CreatePrefab()
+    {
+        GameObject TimePrefab = new GameObject("Clock", typeof(TextMeshPro));
+        TextMeshPro t = TimePrefab.GetComponent<TextMeshPro>();
+        t.text = "TIME";
+        t.enableAutoSizing = true;
+        t.fontSizeMin = 0;
+        t.alignment = TextAlignmentOptions.Center;
+        return TimePrefab;
+    }
+
+    private void Update()
+    {
+        if (updateText)
+        {
+            lastTime = DateTime.Now.TimeOfDay;
+            currentText.text = $"{((lastTime.Hours % 12 ) < 10 ? "0" : "")}{lastTime.Hours % 12}:{(lastTime.Minutes < 10 ? "0" : "")}{lastTime.Minutes} {(lastTime.Hours > 11? "PM": "AM")}";
+        }
+    }
+
+    private void SceneLoaded(VTOLScenes scene)
+    {
+        switch (scene)
+        {
+            case VTOLScenes.SplashScene:
+                break;
+            case VTOLScenes.SamplerScene:
+                break;
+            case VTOLScenes.ReadyRoom:
+                ReadyRoom();
+                break;
+            case VTOLScenes.VehicleConfiguration:
+                break;
+            case VTOLScenes.LoadingScene:
+                break;
+            case VTOLScenes.MeshTerrain:
+                break;
+            case VTOLScenes.OpenWater:
+                break;
+            case VTOLScenes.Akutan:
+                break;
+            case VTOLScenes.VTEditMenu:
+                break;
+            case VTOLScenes.VTEditLoadingScene:
+                break;
+            case VTOLScenes.VTMapEditMenu:
+                break;
+            case VTOLScenes.CustomMapBase:
+                break;
+            case VTOLScenes.CommRadioTest:
+                break;
+            case VTOLScenes.ShaderVariantsScene:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ReadyRoom()
+    {
+        Log("Creating ReadyRoom Clock");
+        currentClock = CreatePrefab();
+        currentText = currentClock.GetComponent<TextMeshPro>();
+        RectTransform rect = currentClock.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(3.7321f, 0.1873f);
+        rect.anchoredPosition3D = new Vector3(-5.992f, -0.9029f, -0.9087f);
+        rect.rotation = Quaternion.Euler(0, -90, 0);
+        updateText = true;
+        Log("Created Clock in ReadyRoom!");
+    }
+
+    public void OnDestory()
+    {
+        VTOLAPI.SceneLoaded -= SceneLoaded;
+    }
+}
